92f86319aaabb46436b5bc7a0e2a8730d111291e
WahajK <wahajkhan788@gmail.com>
2023-03-11 16:07:00 +0500

    Added Limiter to website

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,13 @@
 from website import create_app
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = create_app()
-
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
 if __name__ == "__main__":
     app.run()
