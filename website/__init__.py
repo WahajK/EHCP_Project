40d13dfd38321d019cdb704cdba40cd57219034e
WahajK <wahajkhan788@gmail.com>
2023-05-07 22:28:39 +0500

    Removed and changed secret keys

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+from environment_variables import *
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'icebreaker.asm'
-    app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcBOu0lAAAAAGDeecAdY2kYxSuhah_mQDxOo5y5'
-    app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcBOu0lAAAAABxwJ7Cg8H8Ffc15rqjOj4H5yIZD'
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['RECAPTCHA_PUBLIC_KEY'] = RECAPTCHA_PUBLIC_KEY
+    app.config['RECAPTCHA_PRIVATE_KEY'] = RECAPTCHA_PRIVATE_KEY
     from .views import views
 
     app.register_blueprint(views,url_prefix = '/')
